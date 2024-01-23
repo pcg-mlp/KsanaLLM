7fcbb8eb15999ed6c2f9f9217da3d19e51084f97
ksanallm <ksanallm2024@gmail.com>
2024-01-23 13:51:24 +0800

    fix bugs of attention and nccl layer(merge request !92)
    Squash merge branch 'fix_tensor_para' into 'master'
    1. 修正PagedAttentionLayer输出的shape
    2. 修正NcclAllReduceSumLayer的size和stream

--- src/numerous_llm/layers/nccl_all_reduce_sum_layer.cpp
+++ src/numerous_llm/layers/nccl_all_reduce_sum_layer.cpp
@@ -9,10 +9,10 @@ namespace numerous_llm {
 Status NcclAllReduceSumLayer::Forward(const std::vector<Tensor>& input_tensors, std::vector<Tensor>& output_tensors) {
   if (context_->GetTensorParallelSize() > 1) {
     NCCL_CHECK(ncclGroupStart());
-    ncclResult_t ncclError = ncclAllReduce(reinterpret_cast<const void*>(input_tensors[0].GetPtr<void>()),
-                             output_tensors[0].GetPtr<void>(), input_tensors[0].GetElementNumber() * sizeof(half),
-                             ncclHalf, ncclSum, context_->GetNCCLParam()[rank_].nccl_comm,
-                             context_->GetNCCLStreams()[rank_]);
+    ncclResult_t ncclError =
+        ncclAllReduce(reinterpret_cast<const void*>(input_tensors[0].GetPtr<void>()), output_tensors[0].GetPtr<void>(),
+                      input_tensors[0].GetElementNumber(), ncclHalf, ncclSum, context_->GetNCCLParam()[rank_].nccl_comm,
+                      context_->GetComputeStreams()[rank_]);
 
     if (ncclError != ncclSuccess) {
       NLLM_LOG_INFO << fmt::format("NCCL error: {}\n", ncclGetErrorString(ncclError));
@@ -21,7 +21,8 @@ Status NcclAllReduceSumLayer::Forward(const std::vector<Tensor>& input_tensors, 
   } else {
     void* src = input_tensors[0].GetPtr<void>();
     void* dst = output_tensors[0].GetPtr<void>();
-    CUDA_CHECK(cudaMemcpy(dst, src, input_tensors[0].GetTotalBytes(), cudaMemcpyDeviceToDevice));
+    CUDA_CHECK(cudaMemcpyAsync(dst, src, input_tensors[0].GetTotalBytes(), cudaMemcpyDeviceToDevice,
+                               context_->GetComputeStreams()[rank_]));
   }
   output_tensors[0].shape = input_tensors[0].shape;
   output_tensors[0].dtype = input_tensors[0].dtype;
