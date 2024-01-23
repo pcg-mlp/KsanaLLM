2b273432385918e2d68f1fe73fe4fc90c488f654
ksanallm <ksanallm2024@gmail.com>
2024-01-23 17:45:44 +0800

    add more mainflow document (merge request !93)
    Squash merge branch 'fixed-mainflow-1' into 'master'
    add more mainflow document

--- src/numerous_llm/python/serving_server.py
+++ src/numerous_llm/python/serving_server.py
@@ -2,25 +2,35 @@
 #
 # ==============================================================================
 
-import os
-import sys
+import argparse
 import json
-import torch
+
+from transformers import GenerationConfig
+from transformers import LlamaTokenizer
+from flask import Flask, request, jsonify, make_response
 
 import numerous_llm
 
-from transformers import GenerationConfig
-from transformers import AutoTokenizer, LlamaTokenizer
-from flask import Flask, request, jsonify, make_response
-
 app = Flask('numerous-llm')
 
-# Autokenizer is very slow.
-tokenizer_dir = "/model/llama-hf/13B"
-tokenizer = LlamaTokenizer.from_pretrained(tokenizer_dir)
 
-model_dir = "/model/llama-ft/13B/2-gpu"
-model = numerous_llm.AutoModel.from_pretrained(model_dir)
+def args_config():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model_dir',
+                        type=str,
+                        default="/model/llama-ft/13B/2-gpu",
+                        help='model dir')
+    parser.add_argument('--tokenizer_dir',
+                        type=str,
+                        default="/model/llama-hf/13B",
+                        help='tokenizer dir')
+    parser.add_argument('--host',
+                        type=str,
+                        default="0.0.0.0",
+                        help='server host address')
+    parser.add_argument('--port', type=int, default=8888, help='server port')
+    args = parser.parse_args()
+    return args
 
 
 @app.route('/generate', methods=["POST"])
@@ -32,6 +42,9 @@ def generate():
     prompt_text = json_request["prompt"]
     sampling_config = json_request["sampling_config"]
 
+    tokenizer = app.config["tokenizer"]
+    model = app.config["model"]
+
     input_tokens = tokenizer.encode(prompt_text, add_special_tokens=True)
     generation_config = GenerationConfig(
         num_beams=1,
@@ -42,10 +55,11 @@ def generate():
     try:
         enable_streaming = True
         if enable_streaming:
-            streaming_iter = model.generate(model_name=model_name,
-                                            inputs=input_tokens,
-                                            generation_config=generation_config,
-                                            streamer=True)
+            streaming_iter = model.generate(
+                model_name=model_name,
+                inputs=input_tokens,
+                generation_config=generation_config,
+                streamer=True)
             output_tokens = []
             for output_token in streaming_iter:
                 print("Get step token:", output_token)
@@ -55,12 +69,21 @@ def generate():
             output_tokens = model.generate(model_name=model_name,
                                            inputs=input_tokens,
                                            generation_config=generation_config)
-    except Exception as e:
-        return make_response(jsonify({"texts": str(e)}), 500)
+    except Exception as exc:  # pylint: disable=W0718
+        return make_response(jsonify({"texts": str(exc)}), 500)
 
     output_text = tokenizer.decode(output_tokens)
     return make_response(jsonify({"texts": output_text}), 200)
 
 
-# Use multithread to support parallelism.
-app.run(host="0.0.0.0", port=8888, threaded=True)
+if __name__ == "__main__":
+    args = args_config()
+    app.config["tokenizer_dir"] = args.tokenizer_dir
+    app.config["tokenizer"] = LlamaTokenizer.from_pretrained(
+        app.config["tokenizer_dir"])
+    app.config["model_dir"] = args.model_dir
+    app.config["model"] = numerous_llm.AutoModel.from_pretrained(
+        app.config["model_dir"])
+
+    # Use multithread to support parallelism.
+    app.run(host=args.host, port=args.port, threaded=True)
