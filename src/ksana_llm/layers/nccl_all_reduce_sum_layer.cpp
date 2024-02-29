2b0caa3ac5b44d988c73f55aef2fe13d92e91b78
ksanallm <ksanallm2024@gmail.com>
2024-02-29 13:59:34 +0800

    stream synchronize with event (merge request !123)
    Squash merge branch 'event_sync_1' into 'master'
    performance enhance in batch request
    220 requests
    from Total time: 389.27 s to Total time: 352.51 s
    <img width="" src="/uploads/5BE608DE33BC4226B1291718BDB6CDA6/截屏2024-02-2913.55.15.png" alt="截屏2024-02-2913.55.15.png" />
    
    correctness
    [old.csv](/uploads/75601323639047679CB579F1490EC9BD/old.csv)[new.csv](/uploads/1D83022DE71C44B9A743400DBC8CD41D/new.csv)

--- src/ksana_llm/layers/nccl_all_reduce_sum_layer.cpp
+++ src/ksana_llm/layers/nccl_all_reduce_sum_layer.cpp
@@ -7,12 +7,20 @@
 namespace ksana_llm {
 
 Status NcclAllReduceSumLayer::Forward(const std::vector<Tensor>& input_tensors, std::vector<Tensor>& output_tensors) {
+  // NOTE(karlluo): multiple event in nccl will cause preformance regression
+  // nccl stream just enable when IsRunContextDecodeAndDecodeSerially == false
+  cudaStream_t* stream;
+  if (context_->IsRunContextDecodeAndDecodeSerially()) {
+    stream = &(context_->GetComputeStreams()[rank_]);
+  } else {
+    stream = &(context_->GetNCCLStreams()[rank_]);
+  }
+
   if (context_->GetTensorParallelSize() > 1) {
     NCCL_CHECK(ncclGroupStart());
-    ncclResult_t ncclError =
-        ncclAllReduce(reinterpret_cast<const void*>(input_tensors[0].GetPtr<void>()), output_tensors[0].GetPtr<void>(),
-                      input_tensors[0].GetElementNumber(), ncclHalf, ncclSum, context_->GetNCCLParam()[rank_].nccl_comm,
-                      context_->GetComputeStreams()[rank_]);
+    ncclResult_t ncclError = ncclAllReduce(reinterpret_cast<const void*>(input_tensors[0].GetPtr<void>()),
+                                           output_tensors[0].GetPtr<void>(), input_tensors[0].GetElementNumber(),
+                                           ncclHalf, ncclSum, context_->GetNCCLParam()[rank_].nccl_comm, *stream);
     if (ncclError != ncclSuccess) {
       NLLM_LOG_DEBUG << fmt::format("NCCL error: {}\n", ncclGetErrorString(ncclError));
     }
@@ -20,8 +28,7 @@ Status NcclAllReduceSumLayer::Forward(const std::vector<Tensor>& input_tensors, 
   } else {
     void* src = input_tensors[0].GetPtr<void>();
     void* dst = output_tensors[0].GetPtr<void>();
-    CUDA_CHECK(cudaMemcpyAsync(dst, src, input_tensors[0].GetTotalBytes(), cudaMemcpyDeviceToDevice,
-                               context_->GetComputeStreams()[rank_]));
+    CUDA_CHECK(cudaMemcpyAsync(dst, src, input_tensors[0].GetTotalBytes(), cudaMemcpyDeviceToDevice, *stream));
   }
   output_tensors[0].shape = input_tensors[0].shape;
   output_tensors[0].dtype = input_tensors[0].dtype;
