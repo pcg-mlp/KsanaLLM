e6d9e249c02accc80590eacc7c7a5f65796be645
ksanallm <ksanallm2024@gmail.com>
2024-04-24 13:30:52 +0800

    add more ascend pointwise ut (merge request !224)
    Squash merge branch 'fixed-ascend-issue-2' into 'master'
    add more ut

--- 3rdparty/LLM_kernels/csrc/kernels/ascend/pointwise/pointwise_test.cc
+++ 3rdparty/LLM_kernels/csrc/kernels/ascend/pointwise/pointwise_test.cc
@@ -3,6 +3,7 @@
  */
 
 #include <gtest/gtest.h>
+#include <cmath>
 
 #include "3rdparty/half.hpp"
 #include "csrc/kernels/ascend/pointwise/pointwise.h"
@@ -65,6 +66,187 @@ TEST_F(LlamaAscendPointwiseTestSuit, CastTest) {
   ACL_CHECK_RET(aclrtFree(input_workspace));
   ACL_CHECK_RET(aclrtFree(output_workspace));
   ACL_CHECK_RET(aclrtFree(ref_workspace));
+}
+
+TEST_F(LlamaAscendPointwiseTestSuit, PowTest) {
+  aclTensor* input_tensor = nullptr;
+  void* input_workspace = nullptr;
+  const std::vector<int64_t> input_shape = {1, 2};
+  aclTensor* output_tensor = nullptr;
+  void* output_workspace = nullptr;
+  const std::vector<int64_t> output_shape = {1, 2};
+  CreateAclTensor(input_shape, &input_workspace, aclDataType::ACL_FLOAT16, aclFormat::ACL_FORMAT_ND, &input_tensor);
+  CreateAclTensor(output_shape, &output_workspace, aclDataType::ACL_FLOAT16, aclFormat::ACL_FORMAT_ND, &output_tensor);
+  std::vector<half_float::half> input_vec_host(GetShapeSize(input_shape));
+  std::vector<half_float::half> out_vec_host(GetShapeSize(output_shape));
+  for (size_t i = 0; i < input_vec_host.size(); ++i) {
+    input_vec_host[i] = (half_float::half)(static_cast<float>(rand()) / static_cast<float>(RAND_MAX));
+  }
+  ACL_CHECK_RET(aclrtMemcpyAsync(input_workspace, GetShapeSize(input_shape) * sizeof(half_float::half),
+                                 input_vec_host.data(), GetShapeSize(input_shape) * sizeof(half_float::half),
+                                 ACL_MEMCPY_HOST_TO_DEVICE, stream));
+  Pow(input_tensor, 2.0f, &output_tensor, stream, llm_kernels::utils::GetTestWorkSpaceFunc);
+
+  ACL_CHECK_RET(aclrtMemcpyAsync(out_vec_host.data(), GetShapeSize(input_shape) * sizeof(half_float::half),
+                                 output_workspace, GetShapeSize(output_shape) * sizeof(half_float::half),
+                                 ACL_MEMCPY_DEVICE_TO_HOST, stream));
+  ACL_CHECK_RET(aclrtSynchronizeStream(stream));
+
+  for (size_t i = 0; i < input_vec_host.size(); ++i) {
+    EXPECT_NEAR(float(input_vec_host[i]) * float(input_vec_host[i]), float(out_vec_host[i]), 1e-3);
+  }
+
+  ACL_CHECK_RET(aclDestroyTensor(output_tensor));
+  ACL_CHECK_RET(aclDestroyTensor(input_tensor));
+  ACL_CHECK_RET(aclrtFree(input_workspace));
+  ACL_CHECK_RET(aclrtFree(output_workspace));
+}
+
+TEST_F(LlamaAscendPointwiseTestSuit, NegTest) {
+  aclTensor* input_tensor = nullptr;
+  void* input_workspace = nullptr;
+  const std::vector<int64_t> input_shape = {1, 2};
+  aclTensor* output_tensor = nullptr;
+  void* output_workspace = nullptr;
+  const std::vector<int64_t> output_shape = {1, 2};
+  CreateAclTensor(input_shape, &input_workspace, aclDataType::ACL_FLOAT16, aclFormat::ACL_FORMAT_ND, &input_tensor);
+  CreateAclTensor(output_shape, &output_workspace, aclDataType::ACL_FLOAT16, aclFormat::ACL_FORMAT_ND, &output_tensor);
+  std::vector<half_float::half> input_vec_host(GetShapeSize(input_shape));
+  std::vector<half_float::half> out_vec_host(GetShapeSize(output_shape));
+  for (size_t i = 0; i < input_vec_host.size(); ++i) {
+    input_vec_host[i] = (half_float::half)(static_cast<float>(rand()) / static_cast<float>(RAND_MAX));
+  }
+  ACL_CHECK_RET(aclrtMemcpyAsync(input_workspace, GetShapeSize(input_shape) * sizeof(half_float::half),
+                                 input_vec_host.data(), GetShapeSize(input_shape) * sizeof(half_float::half),
+                                 ACL_MEMCPY_HOST_TO_DEVICE, stream));
+  Neg(input_tensor, &output_tensor, stream, llm_kernels::utils::GetTestWorkSpaceFunc);
+
+  ACL_CHECK_RET(aclrtMemcpyAsync(out_vec_host.data(), GetShapeSize(input_shape) * sizeof(half_float::half),
+                                 output_workspace, GetShapeSize(output_shape) * sizeof(half_float::half),
+                                 ACL_MEMCPY_DEVICE_TO_HOST, stream));
+  ACL_CHECK_RET(aclrtSynchronizeStream(stream));
+
+  for (size_t i = 0; i < input_vec_host.size(); ++i) {
+    EXPECT_NEAR(-(float(input_vec_host[i])), float(out_vec_host[i]), 1e-5);
+  }
+
+  ACL_CHECK_RET(aclDestroyTensor(output_tensor));
+  ACL_CHECK_RET(aclDestroyTensor(input_tensor));
+  ACL_CHECK_RET(aclrtFree(input_workspace));
+  ACL_CHECK_RET(aclrtFree(output_workspace));
+}
+
+TEST_F(LlamaAscendPointwiseTestSuit, MeanTest) {
+  aclTensor* input_tensor = nullptr;
+  void* input_workspace = nullptr;
+  const std::vector<int64_t> input_shape = {1, 2};
+  aclTensor* output_tensor = nullptr;
+  void* output_workspace = nullptr;
+  const std::vector<int64_t> output_shape = {1, 1};
+  std::vector<int64_t> mean_dim_data = {-1};
+  bool keepdim = true;
+  CreateAclTensor(input_shape, &input_workspace, aclDataType::ACL_FLOAT, aclFormat::ACL_FORMAT_ND, &input_tensor);
+  CreateAclTensor(output_shape, &output_workspace, aclDataType::ACL_FLOAT, aclFormat::ACL_FORMAT_ND, &output_tensor);
+  std::vector<float> input_vec_host(GetShapeSize(input_shape));
+  std::vector<float> out_vec_host(GetShapeSize(output_shape));
+  for (size_t i = 0; i < input_vec_host.size(); ++i) {
+    input_vec_host[i] = (static_cast<float>(rand()) / static_cast<float>(RAND_MAX));
+  }
+  ACL_CHECK_RET(aclrtMemcpyAsync(input_workspace, GetShapeSize(input_shape) * sizeof(float), input_vec_host.data(),
+                                 GetShapeSize(input_shape) * sizeof(float), ACL_MEMCPY_HOST_TO_DEVICE, stream));
+  Mean(input_tensor, mean_dim_data, keepdim, aclDataType::ACL_FLOAT, &output_tensor, stream,
+       llm_kernels::utils::GetTestWorkSpaceFunc);
+  ACL_CHECK_RET(aclrtMemcpyAsync(out_vec_host.data(), GetShapeSize(output_shape) * sizeof(float), output_workspace,
+                                 GetShapeSize(output_shape) * sizeof(float), ACL_MEMCPY_DEVICE_TO_HOST, stream));
+  ACL_CHECK_RET(aclrtSynchronizeStream(stream));
+
+  float sum = 0.0f;
+  for (size_t i = 0; i < input_vec_host.size(); ++i) {
+    sum += input_vec_host[i];
+  }
+  EXPECT_NEAR(out_vec_host[0], float(sum / input_vec_host.size()), 1e-3);
+
+  ACL_CHECK_RET(aclDestroyTensor(output_tensor));
+  ACL_CHECK_RET(aclDestroyTensor(input_tensor));
+  ACL_CHECK_RET(aclrtFree(input_workspace));
+  ACL_CHECK_RET(aclrtFree(output_workspace));
+}
+
+TEST_F(LlamaAscendPointwiseTestSuit, InplaceSqrtTest) {
+  aclTensor* input_tensor = nullptr;
+  void* input_workspace = nullptr;
+  const std::vector<int64_t> input_shape = {1, 2};
+  aclTensor* output_tensor = nullptr;
+  void* output_workspace = nullptr;
+  const std::vector<int64_t> output_shape = {1, 2};
+  CreateAclTensor(input_shape, &input_workspace, aclDataType::ACL_FLOAT16, aclFormat::ACL_FORMAT_ND, &input_tensor);
+  CreateAclTensor(output_shape, &output_workspace, aclDataType::ACL_FLOAT16, aclFormat::ACL_FORMAT_ND, &output_tensor);
+  std::vector<half_float::half> input_vec_host(GetShapeSize(input_shape));
+  std::vector<half_float::half> out_vec_host(GetShapeSize(output_shape));
+  for (size_t i = 0; i < input_vec_host.size(); ++i) {
+    input_vec_host[i] = (half_float::half)(static_cast<float>(rand()) / static_cast<float>(RAND_MAX));
+  }
+  ACL_CHECK_RET(aclrtMemcpyAsync(input_workspace, GetShapeSize(input_shape) * sizeof(half_float::half),
+                                 input_vec_host.data(), GetShapeSize(input_shape) * sizeof(half_float::half),
+                                 ACL_MEMCPY_HOST_TO_DEVICE, stream));
+  InplaceSqrt(&input_tensor, stream, llm_kernels::utils::GetTestWorkSpaceFunc);
+
+  ACL_CHECK_RET(aclrtMemcpyAsync(out_vec_host.data(), GetShapeSize(input_shape) * sizeof(half_float::half),
+                                 input_workspace, GetShapeSize(output_shape) * sizeof(half_float::half),
+                                 ACL_MEMCPY_DEVICE_TO_HOST, stream));
+  ACL_CHECK_RET(aclrtSynchronizeStream(stream));
+
+  for (size_t i = 0; i < input_vec_host.size(); ++i) {
+    EXPECT_NEAR(std::sqrt(float(input_vec_host[i])), float(out_vec_host[i]), 1e-3);
+  }
+
+  ACL_CHECK_RET(aclDestroyTensor(output_tensor));
+  ACL_CHECK_RET(aclDestroyTensor(input_tensor));
+  ACL_CHECK_RET(aclrtFree(input_workspace));
+  ACL_CHECK_RET(aclrtFree(output_workspace));
+}
+
+TEST_F(LlamaAscendPointwiseTestSuit, InplaceDivTest) {
+  aclTensor* input_tensor = nullptr;
+  void* input_workspace = nullptr;
+  const std::vector<int64_t> input_shape = {1, 2};
+  aclTensor* output_tensor = nullptr;
+  void* output_workspace = nullptr;
+  const std::vector<int64_t> output_shape = {1, 2};
+  CreateAclTensor(input_shape, &input_workspace, aclDataType::ACL_FLOAT16, aclFormat::ACL_FORMAT_ND, &input_tensor);
+  CreateAclTensor(output_shape, &output_workspace, aclDataType::ACL_FLOAT16, aclFormat::ACL_FORMAT_ND, &output_tensor);
+  std::vector<half_float::half> input_vec_host(GetShapeSize(input_shape));
+  std::vector<half_float::half> out_vec_host(GetShapeSize(output_shape));
+  std::vector<half_float::half> ref_vec_host(GetShapeSize(output_shape));
+  for (size_t i = 0; i < input_vec_host.size(); ++i) {
+    input_vec_host[i] = (half_float::half)(static_cast<float>(rand()) / static_cast<float>(RAND_MAX));
+  }
+  for (size_t i = 0; i < out_vec_host.size(); ++i) {
+    out_vec_host[i] = (half_float::half)(static_cast<float>(rand()) / static_cast<float>(RAND_MAX));
+    ref_vec_host[i] = out_vec_host[i];
+  }
+  ACL_CHECK_RET(aclrtMemcpyAsync(input_workspace, GetShapeSize(input_shape) * sizeof(half_float::half),
+                                 input_vec_host.data(), GetShapeSize(input_shape) * sizeof(half_float::half),
+                                 ACL_MEMCPY_HOST_TO_DEVICE, stream));
+  ACL_CHECK_RET(aclrtMemcpyAsync(output_workspace, GetShapeSize(output_shape) * sizeof(half_float::half),
+                                 out_vec_host.data(), GetShapeSize(output_shape) * sizeof(half_float::half),
+                                 ACL_MEMCPY_HOST_TO_DEVICE, stream));
+  // output_tensor / input_tensor -> output_tensor
+  InplaceDiv(input_tensor, &output_tensor, stream, llm_kernels::utils::GetTestWorkSpaceFunc);
+
+  ACL_CHECK_RET(aclrtMemcpyAsync(out_vec_host.data(), GetShapeSize(input_shape) * sizeof(half_float::half),
+                                 output_workspace, GetShapeSize(output_shape) * sizeof(half_float::half),
+                                 ACL_MEMCPY_DEVICE_TO_HOST, stream));
+  ACL_CHECK_RET(aclrtSynchronizeStream(stream));
+
+  for (size_t i = 0; i < input_vec_host.size(); ++i) {
+    EXPECT_NEAR(float(ref_vec_host[i]) / float(input_vec_host[i]), float(out_vec_host[i]), 1e-3);
+  }
+
+  ACL_CHECK_RET(aclDestroyTensor(output_tensor));
+  ACL_CHECK_RET(aclDestroyTensor(input_tensor));
+  ACL_CHECK_RET(aclrtFree(input_workspace));
+  ACL_CHECK_RET(aclrtFree(output_workspace));
 }
 
 }  // namespace test
