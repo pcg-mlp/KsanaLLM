4933384bbb00ea34ca200badfcffc85652cb58f4
ksanallm <ksanallm2024@gmail.com>
2024-07-03 02:06:14 +0000

    v0.0.19

--- setup.py
+++ setup.py
@@ -103,7 +103,7 @@ class build_ext(build_ext_orig):
 
 
 setup(name='ksana_llm',
-      version='v0.0.18',
+      version='v0.0.19',
       author='ksana_llm',
       author_email='ksana_llm@tencent.com',
       description='Ksana LLM inference server',
