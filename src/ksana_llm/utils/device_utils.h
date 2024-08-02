11bad6d80643d3fcf94b6f8f226a0da734be18d0
ksanallm <ksanallm2024@gmail.com>
2024-08-02 10:16:35 +0800

    replace macro alias with constexpr to avoid name conflicts (merge request !422)

--- src/ksana_llm/utils/device_utils.h
+++ src/ksana_llm/utils/device_utils.h
@@ -19,58 +19,60 @@ using Stream = StreamT<ACTIVE_DEVICE_TYPE>;
 // The event for different device type.
 using Event = EventT<ACTIVE_DEVICE_TYPE>;
 
-}  // namespace ksana_llm
-
 // The event create & destroy.
-#define EventCreate EventCreateT<ACTIVE_DEVICE_TYPE>
-#define EventDestroy EventDestroyT<ACTIVE_DEVICE_TYPE>
+constexpr auto EventCreate = EventCreateT<ACTIVE_DEVICE_TYPE>;
+constexpr auto EventDestroy = EventDestroyT<ACTIVE_DEVICE_TYPE>;
 
 // The event create flag
 #define EVENT_DEFAULT 0x00
 #define EVENT_DISABLE_TIMING 0x02
-#define EventCreateWithFlags EventCreateWithFlagsT<ACTIVE_DEVICE_TYPE>
+constexpr auto EventCreateWithFlags = EventCreateWithFlagsT<ACTIVE_DEVICE_TYPE>;
 
 // The synchronize function.
-#define StreamSynchronize StreamSynchronizeT<ACTIVE_DEVICE_TYPE>
-#define DeviceSynchronize DeviceSynchronizeT<ACTIVE_DEVICE_TYPE>
+constexpr auto StreamSynchronize = StreamSynchronizeT<ACTIVE_DEVICE_TYPE>;
+constexpr auto DeviceSynchronize = DeviceSynchronizeT<ACTIVE_DEVICE_TYPE>;
 
 // The set/get device function.
-#define SetDevice SetDeviceT<ACTIVE_DEVICE_TYPE>
-#define GetDevice GetDeviceT<ACTIVE_DEVICE_TYPE>
+constexpr auto SetDevice = SetDeviceT<ACTIVE_DEVICE_TYPE>;
+constexpr auto GetDevice = GetDeviceT<ACTIVE_DEVICE_TYPE>;
 
-#define GetDeviceCount GetDeviceCountT<ACTIVE_DEVICE_TYPE>
+constexpr auto GetDeviceCount = GetDeviceCountT<ACTIVE_DEVICE_TYPE>;
 
 // The event function.
-#define EventRecord EventRecordT<ACTIVE_DEVICE_TYPE>
-#define StreamWaitEvent StreamWaitEventT<ACTIVE_DEVICE_TYPE>
+constexpr auto EventRecord = EventRecordT<ACTIVE_DEVICE_TYPE>;
+constexpr auto StreamWaitEvent = StreamWaitEventT<ACTIVE_DEVICE_TYPE>;
 
-#define EventSynchronize EventSynchronizeT<ACTIVE_DEVICE_TYPE>
-#define EventElapsedTime EventElapsedTimeT<ACTIVE_DEVICE_TYPE>
+constexpr auto EventSynchronize = EventSynchronizeT<ACTIVE_DEVICE_TYPE>;
+constexpr auto EventElapsedTime = EventElapsedTimeT<ACTIVE_DEVICE_TYPE>;
 
-#define MemGetInfo MemGetInfoT<ACTIVE_DEVICE_TYPE>
+constexpr auto MemGetInfo = MemGetInfoT<ACTIVE_DEVICE_TYPE>;
 
-// The malloc & free
-#define Malloc MallocT<ACTIVE_DEVICE_TYPE>
-#define Free FreeT<ACTIVE_DEVICE_TYPE>
+// The malloc/free function.
+constexpr auto Malloc = MallocT<ACTIVE_DEVICE_TYPE>;
+constexpr auto Free = FreeT<ACTIVE_DEVICE_TYPE>;
 
-// The alloc/free host memory
-#define HostAlloc HostAllocT<ACTIVE_DEVICE_TYPE>
-#define FreeHost FreeHostT<ACTIVE_DEVICE_TYPE>
+// The alloc/free host memory.
+constexpr auto HostAlloc = HostAllocT<ACTIVE_DEVICE_TYPE>;
+constexpr auto FreeHost = FreeHostT<ACTIVE_DEVICE_TYPE>;
 
-// The malloc/free device memory
-#define MallocAsync MallocAsyncT<ACTIVE_DEVICE_TYPE>
-#define FreeAsync FreeAsyncT<ACTIVE_DEVICE_TYPE>
+// The malloc/free device memory.
+constexpr auto MallocAsync = MallocAsyncT<ACTIVE_DEVICE_TYPE>;
+constexpr auto FreeAsync = FreeAsyncT<ACTIVE_DEVICE_TYPE>;
 
 // The memset/memcpy function.
-#define MemsetAsync MemsetAsyncT<ACTIVE_DEVICE_TYPE>
-#define MemcpyAsync MemcpyAsyncT<ACTIVE_DEVICE_TYPE>
+constexpr auto MemsetAsync = MemsetAsyncT<ACTIVE_DEVICE_TYPE>;
+constexpr auto MemcpyAsync = MemcpyAsyncT<ACTIVE_DEVICE_TYPE>;
 
-#define Memset MemsetT<ACTIVE_DEVICE_TYPE>
-#define Memcpy MemcpyT<ACTIVE_DEVICE_TYPE>
+constexpr auto Memset = MemsetT<ACTIVE_DEVICE_TYPE>;
+constexpr auto Memcpy = MemcpyT<ACTIVE_DEVICE_TYPE>;
 
-#define Memcpy2D Memcpy2DT<ACTIVE_DEVICE_TYPE>
-#define Memcpy2DAsync Memcpy2DAsyncT<ACTIVE_DEVICE_TYPE>
+// The memcpy function.
+constexpr auto Memcpy2D = Memcpy2DT<ACTIVE_DEVICE_TYPE>;
+constexpr auto Memcpy2DAsync = Memcpy2DAsyncT<ACTIVE_DEVICE_TYPE>;
 
-#define GetDataType GetDataTypeT<ACTIVE_DEVICE_TYPE>::impl
+template <class U>
+constexpr auto GetDataType = GetDataTypeT<ACTIVE_DEVICE_TYPE>::impl<U>;
 
-#define GetRuntimeContext GetRuntimeContextT<ACTIVE_DEVICE_TYPE>
+constexpr auto GetRuntimeContext = GetRuntimeContextT<ACTIVE_DEVICE_TYPE>;
+
+}  // namespace ksana_llm
